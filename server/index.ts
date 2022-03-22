c6836ccb557e92d20a52c20e88ee480e8dabe269
Victor Winberg <victor.winberg@jayway.com>
2022-03-22 13:54:08 +0100

    add nodemon

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,18 @@
 import express = require('express');
+import path = require('path');
 
 const PORT = Number(process.env.PORT) || 8080;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('🎉 Hello World! 🎉');
+// Serve static files
+app.use(express.static(path.resolve(__dirname,'..', "client", "build")));
+
+app.get('/api', (req, res) => {
+  res.send('🎉 Hello API! 🎉');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
