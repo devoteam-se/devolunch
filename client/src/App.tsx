9448705318e70f1c2a42d9fe9c8531601ffbed0d
Victor Winberg <victor.winberg@jayway.com>
2022-03-23 16:21:20 +0100

    add docker config (#3)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          🎉 Hello Docker! 🎉
         </p>
         <a
           className="App-link"
