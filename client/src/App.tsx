9a92366e0a46a1fc6770f764d8f345dedab8b35f
Victor Winberg <victor.m.winberg@gmail.com>
2022-04-13 12:22:13 +0200

    update app title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          🎉 Hello Docker! 🎉
+          🎉 Devolunch - the greatest lunch app @ devoteam !! 🎉
         </p>
         <a
           className="App-link"
