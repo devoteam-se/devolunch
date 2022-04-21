a9dc1b97b4bf3cc8eb143cf863370d1ac83631f1
Johan Niwhede <niwhede@gmail.com>
2022-04-21 11:33:39 +0200

    Add dev and prod endpoints

--- client/src/contexts/restaurants.tsx
+++ client/src/contexts/restaurants.tsx
@@ -6,7 +6,16 @@ type ContextType = {
   restaurants: App.Restaurant[];
 };
 
+enum Endpoints {
+  RESTAURANTS = "/restaurants",
+}
+
+const API_ROOT_PROD = "https://lunch.jayway.com/api";
+const API_ROOT_DEV = "https://lunch.jayway.com/api";
+
 const RestaurantsContext = React.createContext<ContextType | null>(null);
+
+const isDev = process.env.NODE_ENV === "development";
 
 export const useRestaurants = () => {
   const context = useContext(RestaurantsContext);
@@ -17,8 +26,9 @@ export const useRestaurants = () => {
 };
 
 const RestaurantsProvider = ({ children }: any) => {
+  const rootUrl = isDev ? API_ROOT_DEV : API_ROOT_PROD;
   const { loading, value, error } = useAsync(async () => {
-    const res = await fetch("http://localhost:8080/api/restaurants"); // TODO: Use prod endpoint
+    const res = await fetch(`${rootUrl}${Endpoints.RESTAURANTS}`); // TODO: Use prod endpoint
     const data = await res.json();
     return data;
   });
--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.static(path.resolve(CLIENT_DIR, "build")));
 
 app.get("/api", (req, res) => {
-  res.send('Hello API');
+  res.send("Hello API");
 });
 
 app.get("/api/restaurants", (req, res) => {
@@ -27,74 +27,76 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
 
-
 const mock = [
   {
-    "title": "Spill",
-    "description": "Gourmet sustainable food using recycled food",
-    "imgUrl": "https://media-cdn.tripadvisor.com/media/photo-s/17/fa/8e/94/today-s-special.jpg",
-    "dishes": [
+    title: "Spill",
+    description: "Gourmet sustainable food using recycled food",
+    imgUrl:
+      "https://media-cdn.tripadvisor.com/media/photo-s/17/fa/8e/94/today-s-special.jpg",
+    dishes: [
       {
-        "type": "meat",
-        "description": "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad"
+        type: "meat",
+        description:
+          "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad",
       },
       {
-        "type": "veg",
-        "description": "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad"
-      }
-    ]
+        type: "veg",
+        description:
+          "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad",
+      },
+    ],
   },
   {
-    "title": "Aster",
-    "description": "Local food made gourmet",
-    "imgUrl": "https://media-cdn.tripadvisor.com/media/photo-s/0e/4d/68/73/aster-restaurant.jpg",
-    "dishes": [
+    title: "Aster",
+    description: "Local food made gourmet",
+    imgUrl:
+      "https://media-cdn.tripadvisor.com/media/photo-s/0e/4d/68/73/aster-restaurant.jpg",
+    dishes: [
       {
-        "type": "meat",
-        "description": "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad"
+        type: "meat",
+        description:
+          "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad",
       },
       {
-        "type": "veg",
-        "description": "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad"
-      }
-    ]
+        type: "veg",
+        description:
+          "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad",
+      },
+    ],
   },
   {
-    "title": "Saltimporten",
-    "description": "Food with low impact ",
-    "imgUrl": "https://www.saltimporten.com/media/IMG_6253-512x512.jpg",
-    "dishes": [
+    title: "Saltimporten",
+    description: "Food with low impact ",
+    imgUrl: "https://www.saltimporten.com/media/IMG_6253-512x512.jpg",
+    dishes: [
       {
-        "type": "meat",
-        "description": "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad"
+        type: "meat",
+        description:
+          "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad",
       },
       {
-        "type": "veg",
-        "description": "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad"
-      }
-    ]
+        type: "veg",
+        description:
+          "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad",
+      },
+    ],
   },
   {
-    "title": "Miamarias",
-    "description": "Food made easy and local",
-    "imgUrl": "https://i0.wp.com/www.takemetosweden.be/wp-content/uploads/2019/07/MiaMarias-Malm%C3%B6-1.png?resize=650%2C975&ssl=1",
-    "dishes": [
+    title: "Miamarias",
+    description: "Food made easy and local",
+    imgUrl:
+      "https://i0.wp.com/www.takemetosweden.be/wp-content/uploads/2019/07/MiaMarias-Malm%C3%B6-1.png?resize=650%2C975&ssl=1",
+    dishes: [
       {
-        "type": "meat",
-        "description": "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad"
+        type: "meat",
+        description:
+          "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad",
       },
       {
-        "type": "veg",
-        "description": "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad"
-      }
-    ]
-  }
-]
-
-
-
-
-
-
-
-
+        type: "veg",
+        description:
+          "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad",
+      },
+    ],
+  },
+];
