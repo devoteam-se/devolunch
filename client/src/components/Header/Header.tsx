0695c1ff28fdc2346f04f868e16b9d30661fad4f
Gustav Lilja <lilja.gustav@gmail.com>
2022-04-21 12:26:00 +0200

    fix: adds header and changes font sizes

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <div className="header">
+      <p className="header-text">Lunch time menu</p>
+      <p className="header-text">Icon</p>
+      <p className="header-text">Daily</p>
+    </div>
+  );
+};
