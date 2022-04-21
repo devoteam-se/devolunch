8e81dc148081e01656619a03a7c9298f72db8a6f
pansar1 <7646436+pansar1@users.noreply.github.com>
2022-04-21 10:40:46 +0200

    Add mockdata to api response

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static(path.resolve(CLIENT_DIR, "build")));
 
 app.get("/api", (req, res) => {
-  res.send("🎉 Hello API! 🎉");
+  res.send(mock);
 });
 
 app.get("*", (req, res) => {
@@ -20,3 +20,75 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+
+const mock = [
+  {
+    "title": "Spill",
+    "description": "Gourmet sustainable food using recycled food",
+    "imgUrl": "https://media-cdn.tripadvisor.com/media/photo-s/17/fa/8e/94/today-s-special.jpg",
+    "dishes": [
+      {
+        "type": "meat",
+        "description": "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad"
+      },
+      {
+        "type": "veg",
+        "description": "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad"
+      }
+    ]
+  },
+  {
+    "title": "Aster",
+    "description": "Local food made gourmet",
+    "imgUrl": "https://media-cdn.tripadvisor.com/media/photo-s/0e/4d/68/73/aster-restaurant.jpg",
+    "dishes": [
+      {
+        "type": "meat",
+        "description": "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad"
+      },
+      {
+        "type": "veg",
+        "description": "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad"
+      }
+    ]
+  },
+  {
+    "title": "Saltimporten",
+    "description": "Food with low impact ",
+    "imgUrl": "https://www.saltimporten.com/media/IMG_6253-512x512.jpg",
+    "dishes": [
+      {
+        "type": "meat",
+        "description": "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad"
+      },
+      {
+        "type": "veg",
+        "description": "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad"
+      }
+    ]
+  },
+  {
+    "title": "Miamarias",
+    "description": "Food made easy and local",
+    "imgUrl": "https://i0.wp.com/www.takemetosweden.be/wp-content/uploads/2019/07/MiaMarias-Malm%C3%B6-1.png?resize=650%2C975&ssl=1",
+    "dishes": [
+      {
+        "type": "meat",
+        "description": "Roast beef with mashed potatoes, red wine sauce & tomato / broccoli salad"
+      },
+      {
+        "type": "veg",
+        "description": "Breaded grilled cheese with mashed potatoes, red wine sauce & tomato / broccoli salad"
+      }
+    ]
+  }
+]
+
+
+
+
+
+
+
+
