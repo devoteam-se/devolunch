08247d98b5c81bd81628aa043c9b3d1de27f15c4
Gustav Lilja <lilja.gustav@gmail.com>
2022-04-21 15:37:15 +0200

    feat: adds cooking pot loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,37 @@
-import "./App.css";
-import { Header } from "./components/Header/Header";
-import { RestaurantList } from "./components/RestuarantList";
-import { useRestaurants } from "./contexts/restaurants";
-import { useIsFish } from "./hooks/useIsFish";
+import { useEffect } from 'react';
+import './App.css';
+import { Header } from './components/Header/Header';
+import { RestaurantList } from './components/RestuarantList';
+import { SplashScreen } from './components/SplashScreen/SplashScreen';
+import { useRestaurants } from './contexts/restaurants';
+import { useIsFish } from './hooks/useIsFish';
 
 function App() {
-  const { restaurants } = useRestaurants();
-  // Example with lax
-  const { isFish } = useIsFish("lax");
+  const { restaurants, loading } = useRestaurants();
+
+  //Example with lax
+  const { isFish } = useIsFish('lax');
   console.log(isFish);
+
+  useEffect(() => {
+    if (loading) {
+      const timeout = setTimeout(() => {
+        console.log('got in here');
+      }, 4000);
+      return () => clearTimeout(timeout);
+    }
+  }, [loading]);
 
   return (
     <div className="App">
-      <Header />
-      <RestaurantList restaurants={restaurants} />
+      {loading ? (
+        <SplashScreen />
+      ) : (
+        <>
+          <Header />
+          <RestaurantList restaurants={restaurants} />
+        </>
+      )}
     </div>
   );
 }
