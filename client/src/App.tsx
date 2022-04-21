b7370544bc7b15b4f1f4ba88611381ca7349a373
Gustav Lilja <lilja.gustav@gmail.com>
2022-04-21 15:48:22 +0200

    feat: adds loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,20 +4,16 @@ import { Header } from './components/Header/Header';
 import { RestaurantList } from './components/RestuarantList';
 import { SplashScreen } from './components/SplashScreen/SplashScreen';
 import { useRestaurants } from './contexts/restaurants';
-import { useIsFish } from './hooks/useIsFish';
 
 function App() {
   const { restaurants, loading } = useRestaurants();
 
   //Example with lax
-  const { isFish } = useIsFish('lax');
-  console.log(isFish);
+  // const { isFish } = useIsFish('lax');
 
   useEffect(() => {
     if (loading) {
-      const timeout = setTimeout(() => {
-        console.log('got in here');
-      }, 4000);
+      const timeout = setTimeout(() => {}, 4000);
       return () => clearTimeout(timeout);
     }
   }, [loading]);
