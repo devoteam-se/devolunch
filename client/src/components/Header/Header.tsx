81a1ba0d32d65647cb309e46548d9e4f730bc814
Gustav Lilja <lilja.gustav@gmail.com>
2022-04-21 12:30:16 +0200

    feat: adds header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
+import { ReactComponent as Icon } from '../../assets/sun.svg';
 export const Header = () => {
   return (
     <div className="header">
       <p className="header-text">Lunch time menu</p>
-      <p className="header-text">Icon</p>
+      <Icon className="dish-header-icon" />
       <p className="header-text">Daily</p>
     </div>
   );
