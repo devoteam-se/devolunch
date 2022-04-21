e196c3f598f89b20b645c4bad1f951b28af46929
Victor Winberg <victor.winberg@jayway.com>
2022-04-21 12:01:55 +0200

    add miamarias scraper

--- server/src/scraper.ts
+++ server/src/scraper.ts
@@ -1,4 +1,3 @@
-import { DESTRUCTION } from "dns";
 import puppeteer = require("puppeteer")
 
 const getSlagtHuset = async (page: puppeteer.Page) => {
@@ -18,12 +17,49 @@ const getSlagtHuset = async (page: puppeteer.Page) => {
   })
 }
 
+const getMiaMarias = async (page: puppeteer.Page) => {
+  await page.goto("http://www.miamarias.nu/");
+  return page.evaluate(() => {
+    const dayOfWeek = new Date().getDay();
+    const tables = Array.from(document.querySelectorAll("table"));
+    const raw =
+      tables
+        .slice(dayOfWeek - 1, dayOfWeek)
+        .pop()
+        ?.querySelector("tbody")
+        ?.textContent?.split("\n")
+        .filter((e: string) => e.trim()) || [];
+
+    const dishes = [];
+
+    for (let i = 0; i < raw.length; i += 2) {
+      const type = raw[i]
+        .match(/^[^0-9]+/)
+        ?.shift()
+        ?.trim();
+      const price = Number(raw[i].match(/([0-9]+)\s?kr/)?.slice(1, 2));
+      const description = raw[i + 1];
+      dishes.push({ type, price, description });
+    }
+
+    return {
+      title: "MiaMarias",
+      description: "Idk?",
+      imgUrl: "http://www.miamarias.nu/wp-content/uploads/2018/07/logo_new.png",
+      dishes,
+    };
+  });
+};
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
     
   const slagthuset = await getSlagtHuset(page)
   console.log(slagthuset);
+
+  const miamarias = await getMiaMarias(page)
+  console.log(miamarias);
 
   await browser.close();
 })();
