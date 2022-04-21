5c1898b845b2b4ba06c3200b3498cd2f98684d8a
pansar1 <7646436+pansar1@users.noreply.github.com>
2022-04-21 12:24:56 +0200

    add isFIsh custom hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
 import { RestaurantList } from './components/RestuarantList';
 import { useRestaurants } from './contexts/restaurants';
+import { useIsFish } from "./hooks/useIsFish";
 
 function App() {
   const { restaurants } = useRestaurants();
+  // Example with lax
+  const { isFish } = useIsFish("lax");
+  console.log(isFish)
+
   return (
     <div className="App">
       <RestaurantList restaurants={restaurants} />
