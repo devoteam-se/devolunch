549a0f8548a9563015b6e98adcd63c2d957fa40a
Jonas Stenberg <github@stenberg.io>
2022-04-22 16:31:34 +0200

    Set SLACK_WEBHOOK_URL differently

--- server/src/slack.ts
+++ server/src/slack.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 export default async () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-     await fetch(process.env.SLACK_WEBHOOK, {
+     await fetch(process.env.SLACK_WEBHOOK_URL, {
         method: 'post',
         body: JSON.stringify({
             text: 'Test'
