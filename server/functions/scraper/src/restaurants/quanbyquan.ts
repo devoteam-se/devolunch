3f3e4f0ec885fd9ca65206e669443ad9eb135c94
Jonas Stenberg <github@stenberg.io>
2023-06-29 15:59:26 +0200

    feat: Add quanbyquan

---
+++ server/functions/scraper/src/restaurants/quanbyquan.ts
@@ -0,0 +1,41 @@
+import { Page } from 'puppeteer';
+
+export const meta = {
+  title: 'Quanbyquan',
+  url: 'https://quanbyquan.se/',
+  imgUrl: 'https://quanbyquan.se/wp-content/uploads/2019/09/Quan_Recept_08-1.jpg',
+  googleMapsUrl: 'https://goo.gl/maps/5xyoBjWuU9vUcD6V8',
+  latitude: 55.605634485935816,
+  longitude: 12.997834503074296,
+};
+
+export const browserScrapeFunction = (page: Page) =>
+  page.evaluate(() => {
+    const lunchNodes = [...document.querySelectorAll('div.food_category')];
+
+    if (!lunchNodes) {
+      return [];
+    }
+
+    const lunchNode = lunchNodes
+      .find((m) => m.querySelector('h2')?.innerText.includes('Lunch menu'))
+      ?.querySelectorAll('div.row');
+
+    if (!lunchNode) {
+      return [];
+    }
+
+    return [...lunchNode]
+      .map((a) => (a as HTMLElement).innerText.split('\n').filter((a: string) => a.length && !/^\d{3}:-$/.test(a)))
+      .map((row, index) => {
+        const [title, desc] = row;
+        const lunch = title.charAt(0).toUpperCase() + title.slice(1).toLowerCase() + ' - ' + desc;
+
+        const types = ['misc', 'meat', 'fish', 'meat', 'veg', 'fish'];
+
+        return {
+          type: types[index],
+          description: lunch,
+        };
+      });
+  });
