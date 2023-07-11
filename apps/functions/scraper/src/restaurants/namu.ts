0a80c9f4dd1ccceda48dcb6ecad849609869005b
Jonas Stenberg <github@stenberg.io>
2023-07-11 08:34:29 +0200

    fix: namu image

--- apps/functions/scraper/src/restaurants/namu.ts
+++ apps/functions/scraper/src/restaurants/namu.ts
@@ -3,8 +3,7 @@ import { Page } from 'puppeteer';
 export const meta = {
   title: 'Namu',
   url: 'https://namu.nu/',
-  imgUrl:
-    'https://scontent-arn2-1.cdninstagram.com/v/t51.2885-15/342310447_1320499988499343_7872255442147218804_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=107&_nc_ohc=4Pkb4EZZOcQAX-itCO7&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfA4keYO3xtfCl0Qj7g2Y05rAzC_0J_PfETviYL5mAvfDQ&oe=64A29FEB&_nc_sid=2999b8',
+  imgUrl: 'https://namu.nu/wp-content/uploads/2017/05/Namul-darker2-3k-min.jpg',
   googleMapsUrl: 'https://goo.gl/maps/XtFUKSvmDQTUpR146',
   latitude: 55.605198570165165,
   longitude: 12.997516926554482,
