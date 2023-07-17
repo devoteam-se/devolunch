0c4d7a51301863fb2c86a5eaca20214147275a9a
Jonas Stenberg <github@stenberg.io>
2023-07-17 10:33:40 +0200

    feat: add belle pastabar

---
+++ apps/functions/scraper/src/restaurants/bellepastabar.ts
@@ -0,0 +1,25 @@
+import { Page } from 'puppeteer';
+
+export const meta = {
+  title: 'Belle Pastabar',
+  url: 'https://bennepastabar.se/',
+  imgUrl: 'https://bennepastabar.se/wp-content/themes/benne/images/benne-pastabar-order.jpg',
+  googleMapsUrl: 'https://goo.gl/maps/pZUg6zuXyy6xXxfn7',
+  latitude: 55.60313716015807,
+  longitude: 13.003559388316905,
+  unknownMealDefault: 'veg',
+};
+
+export const browserScrapeFunction = (page: Page) =>
+  page.evaluate(() => {
+    const dishes = [...document.querySelectorAll('div.menu-wrapper h4')]
+      .filter((topic) => topic?.textContent?.trim().length)
+      .slice(0, -1)
+      .map((topic) => ({
+        title:
+          topic.textContent && topic.textContent.charAt(0).toUpperCase() + topic.textContent.slice(1).toLowerCase(),
+        description: topic.nextElementSibling?.textContent,
+      }));
+
+    return dishes;
+  });
