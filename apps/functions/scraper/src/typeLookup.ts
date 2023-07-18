0b6b3467d799b7fe66ae6c9f602aecad2634458e
Jonas Stenberg <github@stenberg.io>
2023-07-18 19:21:11 +0200

    fix: add some cheeky checks to type lookup

--- apps/functions/scraper/src/typeLookup.ts
+++ apps/functions/scraper/src/typeLookup.ts
@@ -7,7 +7,39 @@ export interface OptionProps {
 
 export default (text: string, options: OptionProps = { unknownMealDefault: 'misc' }): DishType => {
   const find = (arr: string[], lookup: string) =>
-    arr.some((keyword: string) => lookup.replace(/\n/g, ' ').toLowerCase().includes(keyword));
+    arr.some((keyword: string) => {
+      // remove full sentences containing 'lägg till' since it's usually
+      // used in the end of a sentence to say something like 'lägg till kött, 10kr'
+      const singleLineLookup = lookup
+        .replace(/\n/g, ' ')
+        .toLowerCase()
+        .split('.')
+        .filter((sentence) => !sentence.includes('lägg till'))
+        .join('.');
+
+      const matchFound = singleLineLookup.includes(keyword);
+
+      if (matchFound) {
+        const keywordPosition = singleLineLookup.indexOf(keyword);
+
+        // we remove the full word before the keyword, for example
+        // if the match is 'fläsk' and the lookup contains 'sidfläsk',
+        // we want to find 'utan' before the full word, not just before 'fläsk'
+        const lineBeforeKeyword = singleLineLookup.substring(0, keywordPosition);
+        const searchString = lineBeforeKeyword.substring(0, lineBeforeKeyword.lastIndexOf(' '));
+
+        const lastWord = searchString
+          .split(' ')
+          .filter((a) => a.length)
+          .pop();
+
+        // don't match in case what we are looking for is prefixed with "utan" (without)
+        if (lastWord === 'utan') {
+          return false;
+        }
+      }
+      return matchFound;
+    });
 
   if (find(keywords.meats, text)) {
     return 'meat';
