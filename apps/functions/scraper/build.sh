8509624c16804ef4d2e0b2d1cc5eab321c5011ec
Jonas Stenberg <github@stenberg.io>
2023-07-18 11:56:10 +0200

    fix: add build step before packing

--- apps/functions/scraper/build.sh
+++ apps/functions/scraper/build.sh
@@ -6,10 +6,12 @@ mkdir -p build
 # Create tarball for both shared and eslint packages
 cd ../../../packages/shared/ || exit
 pnpm install --no-frozen-lockfile
+pnpm build
 pnpm pack
 cp devolunch-shared-1.0.0.tgz ../../apps/functions/scraper/devolunch-shared-1.0.0.tgz
 cd ../eslint || exit
 pnpm install --no-frozen-lockfile
+pnpm build
 pnpm pack
 cp eslint-config-custom-1.0.0.tgz ../../apps/functions/scraper/eslint-config-custom-1.0.0.tgz
 
