e444243a6b97f031bfc29ccd08ca8010905d080e
Jonas Stenberg <github@stenberg.io>
2023-07-18 10:42:13 +0200

    fix: add --no-frozen-lockfile to install step

--- apps/functions/notify-slack/build.sh
+++ apps/functions/notify-slack/build.sh
@@ -5,16 +5,18 @@ mkdir -p build
 
 # Create tarball for both shared and eslint packages
 cd ../../../packages/shared/ || exit
+pnpm install --no-frozen-lockfile
 pnpm pack
 cp devolunch-shared-1.0.0.tgz ../../apps/functions/notify-slack/devolunch-shared-1.0.0.tgz
 cd ../eslint || exit
+pnpm install --no-frozen-lockfile
 pnpm pack
 cp eslint-config-custom-1.0.0.tgz ../../apps/functions/notify-slack/eslint-config-custom-1.0.0.tgz
 
 # Change devDependencies to point to the new tarball and include everything that's built into the zip
 cd ../../apps/functions/notify-slack || exit
 npm pkg set 'devDependencies.@devolunch/shared=file:devolunch-shared-1.0.0.tgz' 'devDependencies.eslint-config-custom=file:eslint-config-custom-1.0.0.tgz'
-pnpm install
+pnpm install --no-frozen-lockfile
 npx make-dedicated-lockfile
 pnpm compile
 cp -R package.json pnpm-lock.yaml dist eslint-config-custom-1.0.0.tgz devolunch-shared-1.0.0.tgz build
